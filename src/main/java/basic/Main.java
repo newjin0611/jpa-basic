8b71674887cd57d3c2f2dc75dbb49fdc02e2c1ba
newjin0611 <newjin0611@gmail.com>
2025-01-27 14:16:32 +0900

    jpa 개발 (maven) - EntityManagerFactory, JPQL, 트랜잭션 안에서 모든 변경 작업 실행 등

--- src/main/java/basic/Main.java
+++ src/main/java/basic/Main.java
@@ -1,7 +1,42 @@
 package basic;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+//            Member member = new Member();
+//
+//            member.setId(2L);
+//            member.setName("HelloB");
+//            em.persist(member);
+//            Member findMember = em.find(Member.class, 1L);
+              List<Member> memberList = em.createQuery("select m from Member as m", Member.class)
+                      .setFirstResult(5)
+                      .setMaxResults(10)
+                      .getResultList();
+              for(Member member : memberList){
+                  System.out.println("member.name : " + member.getName());
+              }
+
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
     }
 }
